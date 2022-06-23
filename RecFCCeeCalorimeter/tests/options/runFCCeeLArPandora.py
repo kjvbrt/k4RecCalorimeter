7439be84dabc8e5014912f381026dbfa8a9c5e8b
Juraj Smiesko <juraj.smiesko@cern.ch>
2022-06-23 13:57:18 +0200

    Small changes to the steering file

--- RecFCCeeCalorimeter/tests/options/runFCCeeLArPandora.py
+++ RecFCCeeCalorimeter/tests/options/runFCCeeLArPandora.py
@@ -1,6 +1,6 @@
 from os import environ, path
 from uuid import uuid4
-from Gaudi.Configuration import INFO, WARNING
+from Gaudi.Configuration import INFO, DEBUG
 
 #
 # Start the Application Manager
@@ -45,22 +45,62 @@ ApplicationMgr().TopAlg += [event_counter]
 #
 from Configurables import GeoSvc
 
-geo_service = GeoSvc("GeoSvc")
-detector_path = environ.get("FCCDETECTORS", "")
-detectors = [
-    'Detector/DetFCCeeIDEA-LAr/compact/FCCee_DectMaster.xml'
-]
-geo_service.detectors = [path.join(detector_path, d) for d in detectors]
-geo_service.OutputLevel = WARNING
+geo_service = GeoSvc('GeoSvc')
+# detector_path = environ.get('FCCDETECTORS', '')
+# detectors = [
+#     'Detector/DetFCCeeIDEA-LAr/compact/FCCee_DectMaster.xml'
+# ]
+# geo_service.detectors = [path.join(detector_path, d) for d in detectors]
+geo_service.detectors = ['/home/jsmiesko/Work/FCC/FCCDetectors/Detector/'
+                         'DetFCCeeIDEA-LAr/compact/FCCee_DectMaster.xml']
+# geo_service.detectors = ['/home/jsmiesko/Work/FCC/FCCDetectors/Detector/'
+#                          'DetFCCeeIDEA/compact/FCCee_DectMaster.xml']
+# geo_service.detectors = ['/home/jsmiesko/Work/FCC/FCCDetectors/Detector/'
+#                          'DetFCCeeCLD/compact/FCCee_o2_v02/FCCee_o2_v02.xml']
+# geo_service.detectors = ['/home/jsmiesko/Work/FCC/lcgeo/FCCee/compact/'
+#                          'FCCee_o1_v04/FCCee_o1_v04.xml']
+geo_service.OutputLevel = DEBUG
 ApplicationMgr().ExtSvc += [geo_service]
+
+### from Configurables import MarlinProcessorWrapper
+
+### detector_path = environ.get('FCCDETECTORS', '')
+### detectors = [
+###     'Detector/DetFCCeeIDEA-LAr/compact/FCCee_DectMaster.xml'
+### ]
+
+### dd4hep = MarlinProcessorWrapper('InitDD4hep')
+### dd4hep.OutputLevel = DEBUG
+### dd4hep.ProcessorType = 'InitializeDD4hep'
+### dd4hep.Parameters = {
+###     'DD4hepXMLFile': [path.join(detector_path, d) for d in detectors],
+### }
+### ApplicationMgr().TopAlg += [dd4hep]
+
+#
+# Pandora in Marlin Wrapper
+#
+from Configurables import MarlinProcessorWrapper
+
+pandora = MarlinProcessorWrapper('DDMarlinPandora')
+pandora.OutputLevel = DEBUG
+pandora.ProcessorType = 'DDPandoraPFANewProcessor'
+pandora.Parameters = {
+    'Verbosity': ['DEBUG2'],
+    'PandoraSettingsXmlFile': ['/some/path'],
+    'CreateGaps': ['False'],
+    'ECalCaloHitCollections': ['ECalBarrelCells']
+}
+ApplicationMgr().TopAlg += [pandora]
+
 
 #
 # Define output root file
 #
 from Configurables import PodioOutput
 
-podio_output = PodioOutput("PodioOutput")
-podio_output.filename = "output_pandora_" + uuid4().hex[:8] + ".root"
+podio_output = PodioOutput('PodioOutput')
+podio_output.filename = 'output_pandora_' + uuid4().hex[:8] + '.root'
 ApplicationMgr().TopAlg += [podio_output]
 
 #
